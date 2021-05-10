a7272cbd8cf72be3a83de451b55d74556cd62d77
Safdar Nazir <safdarnazir14@gmail.com>
2021-05-10 14:58:36 +0500

    Flutter Provider with different flavors.

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_provider/flavor.dart';
+import 'package:flutter_provider/myApp.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    Provider.value(
+      value: Flavor.dev,
+      child: MyApp(),
+    ),
+  );
+}
